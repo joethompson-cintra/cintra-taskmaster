8f5dea8e3e1f1306418f4b20ac0e2450764685e4
Joe Thompson <joe.thompson@cintra.co.uk>
2025-07-04 12:40:41 +0100

    added integration test for get_jira_task and cleaned up old tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,16 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    roots: ['<rootDir>/src'],
     testMatch: ['**/__tests__/**/*.test.ts'],
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        '!src/**/*.d.ts',
+        '!src/**/__tests__/**'
+    ],
+    setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
+    forceExit: true,
+    testTimeout: 30000,
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     transform: {
         '^.+\\.tsx?$': ['ts-jest', {
